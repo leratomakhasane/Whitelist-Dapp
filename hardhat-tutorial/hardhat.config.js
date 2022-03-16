9b6ed3bfa1ae187d60ee3bfa55075b7ac4c61186
Lerato Rampai <87371242+leratomakhasane@users.noreply.github.com>
2022-03-17 00:04:55 +0200

    updated hardhat.config.js file to connect rinkeby network, complied contract successfully

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,21 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({path: ".env"});
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/erH8BevepAEdfQhu7Xg6Z3KORc_3ccRb",
+      accounts: ["0581ece78ce2317088bd9263c20c01c754e09f9908104cc22c75c3c5a63c22f7"],
+    },
+  },
 };
